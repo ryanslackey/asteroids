a588c95fcfa7f26a0c1fe7d4c33567a692f69e7b
ryanslackey <rilackey11@gmail.com>
2025-04-20 10:49:59 -0500

    Added player movement and groups

--- main.py
+++ main.py
@@ -12,14 +12,20 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)    
     player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+        for drawing in drawable:
+            drawing.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
